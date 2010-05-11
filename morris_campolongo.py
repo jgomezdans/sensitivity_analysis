86f1449c65b3a493bb7695e7bf2e709cb9f72bfd
jgomezdans <gomezdansj@gmail.com>
2010-05-11 19:03:16 +0100

    finish morris, and fixed bug with sobol

--- morris_campolongo.py
+++ morris_campolongo.py
@@ -7,7 +7,7 @@ def sobol ( x, a ):
     The Sobol :math:`$g$` function  from Sobol 1990. Requires two parameters, 
     """
     g = (numpy.abs(4.0*x -2 ) + a)/(1+a)
-    return g.product()
+    return g.prod()
 
 def generate_trajectories ( x0, p, delta ):
     """
@@ -53,26 +53,45 @@ def test_generate_trajectories ():
     pdb.set_trace()
 
 def test_morris ():
+    # A test of the Morris SA scheme
+    # Use Sobol's g function, with 6 parameters
+    # Sensitivity of each parameter is
+    # inverseley proportional to parameter value
     a = numpy.array([78., 12., 0.5, 2., 97, 33])
     p = 4
     delta = 2/3.
     k = 2
+    # drange defines the staring points for all
+    # trajectories
     drange = numpy.arange ( 0, 4./3, 1./3)
     B_star = []
+    # Create all trajectories. Define starting point
+    # And calculate trajectory
     for i in itertools.product( drange, drange, drange, \
                                 drange, drange, drange ):
         B_star.append (generate_trajectories ( numpy.array(i), \
             k, delta ) )
+    # B_star contains all our trajectories
     B_star = numpy.array ( B_star )
+    # Next stage: carry out the sensitivity analysis
+    
     ee = []
     for i in xrange(B_star.shape[0]):
+        #for each trajectory, calculate the value of the model
+        # at the starting point
         x0 = B_star[i,0,:]
-        g_pre = sobol ( x0 )
+        g_pre = sobol ( x0, a )
         for j in xrange(1, 7):
+            # Get the new point in the trajectory
             x = B_star[i, j, :]
-            g = sobol( x )
+            #... and calculate the model output
+            g = sobol( x, a )
+            #store the difference. There's a denominator term here
             ee.append( g-g_pre )
+            # Store the current value as the previous for the next
+            # displacement along the trajectory
             g_pre = g
+    # ee contains the distribution. Means and so on
     pdb.set_trace()
 if __name__=="__main__":
     #test_generate_trajectories()
