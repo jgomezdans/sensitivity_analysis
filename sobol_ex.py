3a5c66540ffbf3c264ff2b4e43913973a52c9a5e
jgomezdans <gomezdansj@gmail.com>
2010-05-13 20:10:58 +0100

    itertools fiddling

--- sobol_ex.py
+++ sobol_ex.py
@@ -20,8 +20,8 @@ num_traj = 100
 drange = numpy.arange ( 0, 4./3, 1./3)
 fsobol = lambda x, *a: sobol (x, numpy.array(a))
 (mu_star, mu, sigma) = sensitivity_analysis ( p, k, delta, \
-                    num_traj, drange, \
-                    func=fsobol, args=(a) )
+                    num_traj, drange, r=4, \
+                    func=fsobol, args=(a), sampling="campolongo" )
 Labels=[ r'$x_{%1d}$'%i for i in xrange(1,7)]
 plt.bar(numpy.arange(6)+.5,mu_star, width=0.5, fc='0.8', \
                     label=r'$\mu_{i}^{*}$')
