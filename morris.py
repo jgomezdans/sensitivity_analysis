5545ca8dfdb3159e932d9ad889e6cc638a6c730a
jgomezdans <gomezdansj@gmail.com>
2010-05-13 20:42:08 +0100

    Nested list comprehensions. Is this even right?

--- morris.py
+++ morris.py
@@ -43,7 +43,7 @@ def campolongo_sampling ( b_star, r ):
     k = b_star.shape[2]
     max_dist = 0.
     cnt = 0
-    todo = [[ numpy.sqrt((b_star[m, i, :] - b_star[l, j, :])**2).sum() for ((m,l),i,j) in itertools.product ( itertools.combinations(h,2), range(k), range(k))] for h in intertools.combinations(range(num_traj), r) ]
+    todo = [[ numpy.sqrt((b_star[m, i, :] - b_star[l, j, :])**2).sum() for ((m,l),i,j) in itertools.product ( itertools.combinations(h,2), range(k), range(k))] for h in itertools.combinations(range(num_traj), r) ]
     ###for h in itertools.combinations (range(num_traj), r):
         ###cnt += 1
         ###accum = [ numpy.sqrt((b_star[m, i, :] - b_star[l, j, :])**2).sum() for ((m,l),i,j) in itertools.product ( itertools.combinations(h,2), range(k), range(k))]
@@ -54,10 +54,11 @@ def campolongo_sampling ( b_star, r ):
                         #for z in xrange(k) ]
                 #A = numpy.array( numpy.sqrt (A) ).sum()
                 #accum += A
-        if max_dist < accum:
-            selected_trajectories = h
-        if cnt%100 == 0:
-            print cnt, h
+        #if max_dist < accum:
+            #selected_trajectories = h
+        #if cnt%100 == 0:
+            #print cnt, h
+    pdb.set_trace()
     return b_star[ selected_trajectories, :, :]
             
 def sensitivity_analysis ( p, k, delta, num_traj, drange, \
